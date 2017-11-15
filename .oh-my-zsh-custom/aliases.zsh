11c21b169fc4c18a279471f34cc51bc660b6856b
Olle Vidner <olle@vidner.se>
2017-11-15 13:39:57 +0100

    Added aliases for Python and replaced aliases for virtualenvwrapper with pipenv

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -1,3 +1,8 @@
 alias da=django-admin
 alias dasp="django-admin shell_plus"
-alias wo=workon
+alias ipy=ipython
+alias psh="pipenv shell"
+alias py2=python2
+alias py3=python3
+alias py=py3
+alias work="cd $CODE/personalkollen && psh"
