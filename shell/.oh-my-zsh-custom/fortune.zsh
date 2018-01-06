aa35f8f132417da604d1f6813804bf9137e0ea06
Olle Vidner <olle@vidner.se>
2018-01-06 21:33:26 +0100

    Removed zsh

--- shell/.oh-my-zsh-custom/fortune.zsh
+++
@@ -1,3 +0,0 @@
-if [[ -o login ]]; then
-  print "\e[35m`fortune $ZSH_CUSTOM/fortune/`\n"
-fi
--- shell/.oh-my-zsh-custom/plugins/fzf/fzf.plugin.zsh
+++
@@ -1,3 +0,0 @@
-if [ -x $FZF/bin/fzf ] && [ -x $FZF/bin/fzf-tmux ]; then
-  source "$FZF/shell/key-bindings.zsh"
-fi;
