ba605de62e29a9d0fa26c05a68a15cd15a32f490
Olle Vidner <olle@vidner.se>
2018-01-06 21:33:26 +0100

    Made fish output terminal title

--- shell/config.fish
+++ shell/config.fish
@@ -38,3 +38,11 @@ function fish_right_prompt
 
   echo -n (set_color brblack)$duration
 end
+
+function fish_title
+  test $SSH_TTY
+  and echo -n $USER'@'(prompt_hostname)' '
+  echo -n (prompt_pwd)' '
+  test $argv
+  and echo -n '🚂'$argv
+end
