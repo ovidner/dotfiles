55437fbd407eb5b4d7f0690d6d02bb5fe04efd47
Olle Vidner <olle@vidner.se>
2018-01-06 21:33:26 +0100

    Bootstrap env vars *early*

--- shell/fisher-plugins/ovidner/conf.d/000-ovidner_env.fish
+++ shell/fisher-plugins/ovidner/conf.d/000-ovidner_env.fish
@@ -1,13 +1,14 @@
 set fish_greeting
 set -x CODE $HOME/Code
 set -x LANG en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
 set -x PIPENV_SHELL_FANCY true
 set -x SHELL /usr/local/bin/fish
 set -x VIRTUAL_ENV_DISABLE_PROMPT true
 
 if status --is-login
-  set -gx PATH /usr/local/sbin $PATH
-  set -gx PATH /usr/local/opt/gettext/bin $PATH
-  set -gx PATH $HOME/.local/bin $PATH
+  set -x PATH /usr/local/sbin $PATH
+  set -x PATH /usr/local/opt/gettext/bin $PATH
+  set -x PATH $HOME/.local/bin $PATH
   eval (direnv hook fish)
 end
